b19daefdf3dafcecfc92b5ffe8e9e5a71afcb46c
Daryl <55649534+daryl-sen@users.noreply.github.com>
2020-12-19 01:05:32 -0800

    added kata1 and 2

---
+++ kata2.js
@@ -0,0 +1,17 @@
+const conditionalSum = function(values, condition) {
+  let total = 0;
+  for (let value of values) {
+    if (value % 2 === 0 && condition === 'even') {
+      total += value;
+    }
+    else if (value % 2 !== 0 && condition === 'odd') {
+      total += value;
+    }
+  }
+  return total;
+}
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
